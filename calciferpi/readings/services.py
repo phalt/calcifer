12073470aae9969df886f62583af47bc691930e2
Paul Hallett <paul.hallett@krakentechnologies.ltd>
2025-03-16 12:58:58 +0000

    use single instance

--- calciferpi/readings/services.py
+++ calciferpi/readings/services.py
@@ -5,15 +5,16 @@ def _get_device():
     return get_dht22_device()
 
 
+device = _get_device()
+
+
 def get_temperature() -> float:
-    device = _get_device()
     if device is None:
         return -20.0
     return device.temperature
 
 
 def get_humidity() -> float:
-    device = _get_device()
     if device is None:
         return -50.0
     return device.humidity
